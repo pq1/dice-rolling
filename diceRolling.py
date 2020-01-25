f65e89501b2136e4d044d305217a59b84d5ef897
PQ1 <pq1@users.noreply.github.com>
2020-01-24 20:58:38 -0600

    accommodate other inputs

--- diceRolling.py
+++ diceRolling.py
@@ -1,10 +1,16 @@
 from random import randint
 
-while True:
-    user_input = input('Do you want to roll? Y/N ')
+def rolling():
+  while True:
+      user_input = input('Do you want to roll? Y/N ')
 
-    if user_input.lower() == 'n':
-        print('No more rolling')
-        break
-    else:
-        print(randint(1,6))
+      if user_input.lower() == 'n':
+          print('No more rolling')
+          break
+      elif user_input.lower() == 'y':
+          print(randint(1,6))
+      # User enters anything other than y or n
+      else:
+          print('Please enter in a Y or N')
+
+rolling()
